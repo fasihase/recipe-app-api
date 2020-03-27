51b472672027f03717680947d3fe00ca1fd5a63f
Fasih <FasihA.SE@gmail.com>
2020-03-28 01:25:09 +1100

    added manage user endpoint

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,7 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Third party
+    'rest_framework',
+    'rest_framework.authtoken',
+    # Local
     'core.apps.CoreConfig',
+    'user.apps.UserConfig',
 ]
 
 MIDDLEWARE = [
